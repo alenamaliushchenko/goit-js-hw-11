766fd0fa99bad6fa061b7493ddc1b60bcfd7ccfa
Alena Maliushchenko <alenamaliushchenko@gmail.com>
2024-07-12 13:11:34 +0300

    changed

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,26 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+export async function fetchImages(query){
+    const apiKEY = '44883065-b38075931b52680ca8f625c61';
+    const url ='https://pixabay.com/api/?key=${apiKey}&q=${encodeURLComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true';
+
+return fetch(url)
+    .then(response => {
+        if(!response.ok) {
+            throw new Error(response.status);
+        }
+        return response.json();
+    })
+    .then(data => {
+        return data;
+    })
+    .catch(error => {
+        console.log('Error:', error);
+        iziToast.error({
+            title: 'Error',
+            message: 'Sorry, there are no images matching your search query. Please try again!',
+        });
+        throw error;
+    });
+}
---
+++ src/main.js
@@ -0,0 +1,31 @@
+import { fetchImages } from "./js/pixabay-api";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+document.getElementById('searchForm').addEventListener("submit", function(event) {
+    event.preventDefault();
+    const query = document.getElementById('searchInput').trim().value;
+
+if (query === ""){
+    iziToast.error({
+        title: 'Error',
+        message: 'Sorry, there are no images matching your search query. Please try again!',
+    });
+    return;
+}
+fetchImages(query) 
+   then(data => {
+    if(data.hits.length > 0){
+       return data.hits;
+    }
+    else{
+        iziToast.error({
+            title: 'Error',
+            message: 'Sorry, there are no images matching your search query. Please try again!',
+        });
+    }
+   })
+   .catch(error => {
+   });
+  
+})
